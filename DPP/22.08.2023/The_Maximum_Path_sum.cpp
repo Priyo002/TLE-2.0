521cf5cb1b80a0b1643a31f3f1078cbf5194a441
Priyabrata Das <priyobapi@gmail.com>
2023-08-22 22:39:16 +0530

    Update

---
+++ DPP/22.08.2023/The_Maximum_Path_sum.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define pb push_back
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define endl '\n'
+#define sort(X) sort(X.begin(),X.end())
+const int mod = 1e9 + 7;
+const int INF = 1e18;
+
+void init() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("inputf.in", "r", stdin);
+	freopen("outputf.in", "w", stdout);
+#endif
+}
+
+int n, m;
+int arr[15][15];
+int solve(int i, int j) {
+	if (i == n || j == m)
+		return 0;
+	return arr[i][j] + max(solve(i + 1, j) + solve(i, j + 1));
+
+}
+
+int32_t main() {
+
+	init();
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	cout << solve(0, 0);
+
+
+}
